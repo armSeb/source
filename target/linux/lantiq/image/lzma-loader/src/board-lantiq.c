b589956cc352a073182fc8708da4032172a066ab
Sebastien Decourriere <sebtx452@gmail.com>
2017-12-06 18:09:52 +0100

    Ethernet init hack.....
    Signed-off-by: Sebastien Decourriere <sebtx452@gmail.com>

--- target/linux/lantiq/image/lzma-loader/src/board-lantiq.c
+++ target/linux/lantiq/image/lzma-loader/src/board-lantiq.c
@@ -9,10 +9,13 @@
  */
 
 #include <stddef.h>
+#include <stdint.h>
 #include "config.h"
 
 #define READREG(r)		*(volatile unsigned int *)(r)
+#define READREG32(r)		*(volatile uint32_t *)(r)
 #define WRITEREG(r,v)		*(volatile unsigned int *)(r) = v
+#define WRITEREG32(r,v)		*(volatile uint32_t *)(r) = v
 
 #define UART_BASE		0xbe100c00
 #define ASC_TBUF		(UART_BASE | 0x20)
@@ -32,6 +35,15 @@
 #define LTQ_WDT_DIVIDER         0x40000
 #define LTQ_MAX_TIMEOUT         ((1 << 16) - 1) /* the reload field is 16 bit */
 
+#define RCU_MEMBASE			0xBF203000
+/* reboot bit */
+#define RCU_RD_GPHY0_XRX200     (1<<31)
+#define RCU_RD_SRST             (1<<30)
+#define RCU_RD_GPHY1_XRX200     (1<<29)
+#define RCU_RST_REQ             0x0010
+
+#define PMU_BASE		0xBF102000
+#define BSP_PMU_PWDCR		0x001C
 
 #define TXMASK          0x3F00
 #define TXOFFSET        8
@@ -49,5 +61,20 @@ void board_init(void)
 	// WDT reset timer
 	WRITEREG(LTQ_WDT_MEMBASE_CR, LTQ_WDT_PW1); // First magic word
 	WRITEREG(LTQ_WDT_MEMBASE_CR, LTQ_WDT_SR_EN | LTQ_WDT_SR_PWD | LTQ_WDT_SR_CLKDIV | LTQ_WDT_PW2 | LTQ_MAX_TIMEOUT);
+
+	uint32_t val = READREG32(RCU_MEMBASE);
+	printf("%08X\n", val);
+	if ((val&0x04000000) != 0x04000000) {
+		val=READREG32(RCU_MEMBASE + RCU_RST_REQ);
+		val |= RCU_RD_GPHY1_XRX200 | RCU_RD_GPHY0_XRX200 | RCU_RD_SRST;
+		WRITEREG32(RCU_MEMBASE + RCU_RST_REQ, val);
+
+	}
+//	val |= RCU_RD_GPHY1_XRX200 | RCU_RD_GPHY0_XRX200 | RCU_RD_SRST;
+//	val &= 0xCFFFFFDF;
+//	WRITEREG32(PMU_BASE + BSP_PMU_PWDCR, val);
+//	printf("%08X\n", val);
+
+
 //	WRITEREG(0xbe105360, 0x000111A7);
 }
