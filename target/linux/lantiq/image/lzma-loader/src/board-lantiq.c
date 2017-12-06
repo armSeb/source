062bf9fbaf626547f17ac0b161933877c64f8afa
Sebastien Decourriere <sebtx452@gmail.com>
2017-12-06 10:54:38 +0100

    Reset WDT timer instead of deactivating
    Signed-off-by: Sebastien Decourriere <sebtx452@gmail.com>

--- target/linux/lantiq/image/lzma-loader/src/board-lantiq.c
+++ target/linux/lantiq/image/lzma-loader/src/board-lantiq.c
@@ -18,6 +18,21 @@
 #define ASC_TBUF		(UART_BASE | 0x20)
 #define ASC_FSTAT		(UART_BASE | 0x48)
 
+// Watchdog
+
+#define LTQ_WDT_MEMBASE_CR	0xbf8803f0 // Membase + control register
+
+#define LTQ_WDT_PW1             0x00BE0000
+#define LTQ_WDT_PW2             0x00DC0000
+
+#define LTQ_WDT_SR_EN           (0x1 << 31)     /* enable bit */
+#define LTQ_WDT_SR_PWD          (0x3 << 26)     /* turn on power */
+#define LTQ_WDT_SR_CLKDIV       (0x3 << 24)     /* turn on clock and set */
+                                                /* divider to 0x40000 */
+#define LTQ_WDT_DIVIDER         0x40000
+#define LTQ_MAX_TIMEOUT         ((1 << 16) - 1) /* the reload field is 16 bit */
+
+
 #define TXMASK          0x3F00
 #define TXOFFSET        8
 
@@ -30,10 +45,9 @@ void board_putc(char c)
 
 void board_init(void)
 {
-	/* Make sure the watchdog is disabled
-	   by writing the magic words	 */
-	WRITEREG(0xbf8803f0, 0x00be0000);
-	WRITEREG(0xbf8803f0, 0x00dc0000);
 
+	// WDT reset timer
+	WRITEREG(LTQ_WDT_MEMBASE_CR, LTQ_WDT_PW1); // First magic word
+	WRITEREG(LTQ_WDT_MEMBASE_CR, LTQ_WDT_SR_EN | LTQ_WDT_SR_PWD | LTQ_WDT_SR_CLKDIV | LTQ_WDT_PW2 | LTQ_MAX_TIMEOUT);
 //	WRITEREG(0xbe105360, 0x000111A7);
 }
