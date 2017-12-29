59d1d2943303a2c77a4ce1247a9bda6fe1635edf
Sebastien Decourriere <sebtx452@gmail.com>
2017-12-29 10:52:55 +0100

    voila
    Signed-off-by: Sebastien Decourriere <sebtx452@gmail.com>

--- target/linux/lantiq/image/lzma-loader/src/board-lantiq.c
+++ target/linux/lantiq/image/lzma-loader/src/board-lantiq.c
@@ -10,7 +10,9 @@
 
 #include <stddef.h>
 #include <stdint.h>
+#include <unistd.h>
 #include "config.h"
+
 
 #define READREG(r)		*(volatile unsigned int *)(r)
 #define READREG32(r)		*(volatile uint32_t *)(r)
@@ -42,8 +44,12 @@
 #define RCU_RD_GPHY1_XRX200     (1<<29)
 #define RCU_RST_REQ             0x0010
 
+#define CGU_BASE_ADDR		0xBF103000
+
 #define PMU_BASE		0xBF102000
 #define BSP_PMU_PWDCR		0x001C
+#define PMU_PWDCR 		0xBF10201C
+#define PMU_SR			0xBF102020
 
 #define TXMASK          0x3F00
 #define TXOFFSET        8
@@ -62,20 +68,44 @@ void board_init(void)
 	WRITEREG(LTQ_WDT_MEMBASE_CR, LTQ_WDT_PW1); // First magic word
 	WRITEREG(LTQ_WDT_MEMBASE_CR, LTQ_WDT_SR_EN | LTQ_WDT_SR_PWD | LTQ_WDT_SR_CLKDIV | LTQ_WDT_PW2 | LTQ_MAX_TIMEOUT);
 
+
+ printf("%08X\n", READREG32(0xBF10201C));
+// printf("%08X %08X\n", READREG32(0xBE109138), READREG32(0xBE10913C));
+// printf("%08X %08X\n", READREG32(0xBE10B308), READREG32(0xBE10B1DC));
+// printf("%08X %08X\n", READREG32(0xBE10B1E4), READREG32(0xBE10B204));
+
+//	WRITEREG(CGU_BASE_ADDR+0x44, 0x1FE00000);
+//	WRITEREG(0xBF103020, 0x01);
+//	WRITEREG(RCU_RST_REQ+RCU_MEMBASE, RCU_RD_GPHY0_XRX200 | RCU_RD_GPHY0_XRX200);
+/*	uint32_t lu = READREG32(0xBF10201C);
+	WRITEREG32(0xBF10201C, 0x02);
+	printf("%08X\n", READREG32(0xBF10201C));
+	WRITEREG32(PMU_PWDCR, 0x0e1592cf);
+	printf("%08X\n", READREG32(0xBF10201C));
+	lu = READREG32(0xBF203010);
+	printf("%08X\n", lu);
+	WRITEREG32(0xBF203010,lu | 0x200300);
+	lu = READREG32(0xBF203010);
+	printf("%08X\n", lu);
+	WRITEREG32(0xBF203010, lu & 0xFFDFFCFF);
+	printf("%08X\n", READREG(0xBF203010));
+*/
+//	WRITEREG(0xBF203010, 0x40000008);
+
+/*	uint32_t n=0;
+	while (n<999999999) n++;
+	WRITEREG(0xBF203010, 0x20);*/
+//	WRITEREG(0xBF401070, 0x1010100);
+
+
+
 	uint32_t val = READREG32(RCU_MEMBASE);
 	uint32_t gpio = READREG32(0xBE100B10);
 	printf("Reset status: %08X, gpio: %08X\n", val, gpio);
-/*	if ((val&0x04000000) != 0x04000000) {
-		val=READREG32(RCU_MEMBASE + RCU_RST_REQ);
-		val |= RCU_RD_GPHY1_XRX200 | RCU_RD_GPHY0_XRX200 | RCU_RD_SRST;
-		WRITEREG32(RCU_MEMBASE + RCU_RST_REQ, val);
 
-	}*/
-//	val |= RCU_RD_GPHY1_XRX200 | RCU_RD_GPHY0_XRX200 | RCU_RD_SRST;
-//	val &= 0xCFFFFFDF;
-//	WRITEREG32(PMU_BASE + BSP_PMU_PWDCR, val);
-//	printf("%08X\n", val);
-
+	// WDT reset timer
+        WRITEREG(LTQ_WDT_MEMBASE_CR, LTQ_WDT_PW1); // First magic word
+        WRITEREG(LTQ_WDT_MEMBASE_CR, LTQ_WDT_SR_EN | LTQ_WDT_SR_PWD | LTQ_WDT_SR_CLKDIV | LTQ_WDT_PW2 | LTQ_MAX_TIMEOUT);
 
 //	WRITEREG(0xbe105360, 0x000111A7);
 }
